ae6442b65d21d30765453f899b2a7687f339d15d
Ida Andersson <andson_4id@hotmail.com>
2023-02-09 15:02:20 +0100

    Bootstrap/navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+<nav class="navbar bg-body-tertiary fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tour</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+<style scoped>
+
+#offcanvasNavbar {
+width: 5wv
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import './assets/main.css'
 
 const app = createApp(App)
 
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
 app.use(router)
+app.use(bootstrap)
 
 app.mount('#app')
